f7652ef7ebb2d369536426814aafcf37378f982b
ryanhaglund <ryanhaglund@gmail.com>
2014-01-23 09:16:02 -0800

    added title move

--- Assets/Scripts/Ryan/MenuAnchorMoveX.cs
+++ Assets/Scripts/Ryan/MenuAnchorMoveX.cs
@@ -1,21 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MenuAnchorMove : MonoBehaviour 
+public class MenuAnchorMoveX : MonoBehaviour 
 {
 	public GameObject move;
 	public float seconds;
 	public float timeDelay;
 	public float speed;
 	private UIAnchor anchor;
-	//private Vector2 screen;
 
 	
 	void Start()
 	{
 		anchor = move.GetComponent<UIAnchor> ();
-		//screen = new Vector2 (Screen.width, Screen.height);
-		//anchor.relativeOffset.x = 0.3f;
 	}
 	void Update()
 	{
@@ -27,9 +24,6 @@ public class MenuAnchorMove : MonoBehaviour
 		{
 			anchor.relativeOffset.x += speed;
 		}
-
-		//anchor.relativeOffset.x += 0.01f;
-		Debug.Log (anchor.relativeOffset.x);
 	}
 	
 }
