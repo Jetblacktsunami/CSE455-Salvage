eac43f6a09a519c9c8e32d30c1291a72ada053c3
marques stone <003579276@coyote.csusb.edu>
2014-02-09 17:11:40 -0800

    healthbar for ship commit
    i have the start for the healthbar part of the ui it doesn't compile yet
    because of a few syntac errors that im trying to figure out.

---
+++ Assets/Scripts/Marques/gui.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+int GameObject target;
+
+public class gui : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyUp(KeyCode.F)){
+			Attack();
+		}
+	}
+
+	public void Attack () {
+		<ui.cs> eh = (<ui.cs>)target.GetComponent("<ui.cs>");
+		eh.AdjustcurHealth(-1);
+	}
+
+}
