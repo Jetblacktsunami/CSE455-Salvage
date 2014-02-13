15a7a30a301e183971a12cc34e718055bcb732cc
JetBlackTsunami <parkerg718@gmail.com>
2014-02-12 17:49:47 -0800

    Adding a game manager

---
+++ Assets/Scripts/Parker/Managers/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class GameManager : MonoBehaviour 
+{
+	public static Action<FunctionCallType> gameManagerCalls;
+
+	public enum FunctionCallType{ load, save, exit };
+	private FunctionCallType functionCall = FunctionCallType.load;
+
+	void Start()
+	{
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	void OnLevelWasLoaded()
+	{
+		if(Application.loadedLevelName == "InGame")
+		{
+
+		}
+	}
+
+	void Save()
+	{
+
+	}
+
+
+}
