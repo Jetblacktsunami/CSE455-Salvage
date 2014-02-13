80c4e315fc7a12cea3ff651d6d0462a674edc46d
Jared Johnson <johnj351@coyote.csusb.edu>
2014-02-12 20:13:37 -0800

    PlayerInfo update

--- Assets/Scripts/Jared/PlayerInformation.cs
+++ Assets/Scripts/Jared/PlayerInformation.cs
@@ -1,12 +1,13 @@
 using UnityEngine;
 using System.IO;
 using System.Xml;
+using System.Xml.Linq;
 using System.Collections;
 using System.Collections.Generic;
 
 public class PlayerInformation : MonoBehaviour 
 {
-   private string deviceID;
+	private string deviceID;
 
 	private string ship;
 	private int maxHealth;
@@ -47,7 +48,22 @@ public class PlayerInformation : MonoBehaviour
 		}
 #endif
 	}
-	
+
+	void Awake()
+	{		
+		DontDestroyOnLoad(gameObject);
+		
+		if(File.Exists(savePath))
+		{
+			LoadData();
+		}
+		else
+		{			
+			SaveData();
+		}
+	}
+
+	//Accessor functions
 	public string getShip()
 	{
 		return ship;
@@ -137,7 +153,8 @@ public class PlayerInformation : MonoBehaviour
 	{
 		return weaponFireRate;
 	}
-	
+
+	//Mutator functions
 	public void setShip(string newShip)
 	{
 		ship = newShip;
@@ -227,7 +244,8 @@ public class PlayerInformation : MonoBehaviour
 	{
 		weaponFireRate = newRate;
 	}
-		
+
+	//Other functions
 	public void applyDamage(int damage)
 	{
 		if(currentShields > 0)
@@ -256,5 +274,155 @@ public class PlayerInformation : MonoBehaviour
 			}
 		}
 		currentFuel -= fuelConsumptionRate;
+	}
+
+	public void SaveData()
+	{
+		Debug.Log("Saving...");
+		if(File.Exists(savePath))
+		{
+			File.Delete(savePath);
+		}
+
+		XmlWriter writer = new XmlTextWriter(savePath, System.Text.Encoding.UTF8);
+		writer.WriteStartDocument();
+		writer.WriteWhitespace("\n");
+		writer.WriteStartElement("Root");
+		writer.WriteWhitespace("\n\t");
+		writer.WriteElementString("deviceID", SystemInfo.deviceUniqueIdentifier);
+		writer.WriteWhitespace("\n\t");
+		writer.WriteElementString("ship", ship);
+		writer.WriteWhitespace("\n\t");
+		writer.WriteElementString("maxHealth", maxHealth.ToString());
+		writer.WriteWhitespace("\n\t");
+		writer.WriteElementString("currentHealth", currentHealth.ToString());
+		writer.WriteWhitespace("\n\t");
+		writer.WriteElementString("maxShields", maxShields.ToString());
+		writer.WriteWhitespace("\n\t");
+		writer.WriteElementString("currentShields", currentShields.ToString());
+		writer.WriteWhitespace("\n\t");
+		writer.WriteElementString("shieldRechargeDelay", shieldRechargeDelay.ToString());
+		writer.WriteWhitespace("\n\t");
+		writer.WriteElementString("shieldRechargeRate", shieldRechargeRate.ToString());
+		writer.WriteWhitespace("\n\t");
+		writer.WriteElementString("maxFuel", maxFuel.ToString());
+		writer.WriteWhitespace("\n\t");
+		writer.WriteElementString("currentFuel", currentFuel.ToString());
+		writer.WriteWhitespace("\n\t");
+		writer.WriteElementString("fuelConsumptionRate", fuelConsumptionRate.ToString());
+		writer.WriteWhitespace("\n\t");
+		writer.WriteElementString("acceleration", acceleration.ToString());
+		writer.WriteWhitespace("\n\t");
+		writer.WriteElementString("speed", speed.ToString());
+		writer.WriteWhitespace("\n\t");
+		writer.WriteElementString("maxSpeed", maxSpeed.ToString());
+		writer.WriteWhitespace("\n\t");
+		writer.WriteElementString("thrusterDelay", thrusterDelay.ToString());
+		writer.WriteWhitespace("\n\t");
+		writer.WriteElementString("rotationSpeed", rotationSpeed.ToString());
+		writer.WriteWhitespace("\n\t");
+		writer.WriteElementString("weapon", weapon);
+		writer.WriteWhitespace("\n\t");
+		writer.WriteElementString("weaponDamage", weaponDamage.ToString());
+		writer.WriteWhitespace("\n\t");
+		writer.WriteElementString("weaponFireRate", weaponFireRate.ToString());
+		writer.WriteEndElement();
+		writer.Close();
+
+		XMLFileManager.EncryptFile(savePath);
+	}
+
+	public void LoadData()
+	{
+		Debug.Log("Loading...");
+		if(!File.Exists(savePath))
+		{
+			return;
+		}
+		else
+		{
+			XMLFileManager.DecryptFile(savePath);
+		}
+
+		XmlReader reader = new XmlTextReader(savePath);
+		while(reader.Read())
+		{
+			if(reader.IsStartElement() && reader.NodeType == XmlNodeType.Element)
+			{
+				switch(reader.Name)
+				{
+					case "deviceID":
+						Debug.Log(deviceID + "\n we found the id");
+						deviceID = reader.ReadElementString();
+						break;
+					case "ship":
+						ship = reader.ReadElementString();
+						break;
+					case "maxHealth":
+						maxHealth = int.Parse(reader.ReadElementString());
+						break;
+					case "currentHealth":
+						currentHealth = int.Parse(reader.ReadElementString());
+						break;
+					case "maxShields":
+						maxShields = int.Parse(reader.ReadElementString());
+						break;
+					case "currentShields":
+						currentShields = int.Parse(reader.ReadElementString());
+						break;
+					case "shieldRechargeDelay":
+						shieldRechargeDelay = int.Parse(reader.ReadElementString());
+						break;
+					case "shieldRechargeRate":
+						shieldRechargeRate = int.Parse(reader.ReadElementString());
+						break;
+					case "maxFuel":
+						maxFuel = float.Parse(reader.ReadElementString());
+						break;
+					case "currentFuel":
+						currentFuel = float.Parse(reader.ReadElementString());
+						break;
+					case "fuelConsumptionRate":
+						fuelConsumptionRate = float.Parse(reader.ReadElementString());
+						break;
+					case "acceleration":
+						acceleration = float.Parse(reader.ReadElementString());
+						break;
+					case "speed":
+						speed = float.Parse(reader.ReadElementString());
+						break;
+					case "maxSpeed":
+						maxSpeed = float.Parse(reader.ReadElementString());
+						break;
+					case "thrusterDelay":
+						thrusterDelay = float.Parse(reader.ReadElementString());
+						break;
+					case "rotationSpeed":
+						rotationSpeed = float.Parse(reader.ReadElementString());
+						break;
+					case "weapon":
+						weapon = reader.ReadElementString();
+						break;
+					case "weaponDamage":
+						weaponDamage = int.Parse(reader.ReadElementString());
+						break;
+					case "weaponFireRate":
+						weaponFireRate = float.Parse(reader.ReadElementString());
+						break;
+					default:
+						break;
+				}
+			}
+		}
+
+		reader.Close();
+
+		if(deviceID != SystemInfo.deviceUniqueIdentifier)
+		{
+			SaveData();
+			Debug.Log("Invalid ID for Loading");
+		}
+
+		XMLFileManager.EncryptFile(savePath);
 	}
 }
