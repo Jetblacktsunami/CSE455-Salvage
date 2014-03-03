d26bf5a58ccec15fa33dfd95484e4d652f26f815
marques stone <003579276@coyote.csusb.edu>
2014-03-03 12:34:47 -0800

    explosions for ship and asteroids
    i am committing a scene with a few extra script in order to create the
    explosions for both the asteroid as well as the ship i also messed
    around with the background and stuff just to learn from the unity
    tutorials. still isn't complete because it wasn't working some Vector3
    error.

---
+++ Assets/Scripts/Marques/random_rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class random_rotator : MonoBehaviour 
+{
+	public float tumble;
+
+	void start()
+	{
+		rigidbody.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+}
